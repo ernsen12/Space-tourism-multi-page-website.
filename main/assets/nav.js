35ac2607bc91ab5e49d3ca8f6f02916c7eb1df91
ernsen12 <princelicup@gmail.com>
2023-07-31 00:51:57 +0800

    update

--- main/assets/nav.js
+++ main/assets/nav.js
@@ -7,13 +7,17 @@ navIcon.on("click", () => {
 });
 
 const navActive = $(".active_nav");
-const navLink = $(".nav_link");
 const navAnchor = $(".nav_text");
 
-navAnchor.each(function(){
-    if ($(this).prop('href') == window.location.href) {
-        navActive.removeClass( "active_nav");
-        $(this).parents('li').addClass('active_nav');
-    }
-});
+function navIndicator(element, index){
+    element.each(function(){
+        if ($(this).prop('href') == window.location.href){
+            navActive.removeClass( "active_nav");
+            $(navAnchor[index]).parents('li').addClass('active_nav');
+        }
+    });
+};
 
+navIndicator($(".planet_nav a"), 1);
+navIndicator($(".crew_link"), 2);
+navIndicator($(".tech_link"), 3);
